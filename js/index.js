f0e16e96664ff4c73dd37f5b7acfebeaec451735
Konrad Kuceł <konradkucel@gmail.com>
2019-04-16 19:21:42 +0200

    Add modals

--- js/index.js
+++ js/index.js
@@ -1,74 +1,109 @@
 'use strict';
 
-/*var buttonNewGame = document.getElementById('new-game');
+var buttonNewGame = document.getElementById('new-game');
 var buttonPaper = document.getElementById('paper');
 var buttonRock = document.getElementById('rock');
 var buttonScissors = document.getElementById('scissors');
-
 var numberOfGames = document.getElementById('games');
-var params.output = document.getElementById('output');
+var output = document.getElementById('output');
 var outputResult = document.getElementById('result');
-var outputEndOfGame = document.getElementById('end-game');*/
+var outputEndOfGame = document.getElementById('end-game');
+var modal = document.getElementById('modal');
 
-var params = { 
-  buttonNewGame: document.getElementById('new-game'),
-  buttonPaper: document.getElementById('paper'),
-  buttonRock: document.getElementById('rock'),
-  buttonScissors: document.getElementById('scissors'), 
-  numberOfGames: document.getElementById('games'),
-  output: document.getElementById('output'),
-  outputResult: document.getElementById('result'),
-  outputEndOfGame: document.getElementById('end-game'),
+var player = {
+  score: 0,
+  choice: ''
+};
+
+var computer = {
+  score: 0,
+  choice: ''
+};
+
+var params = {
   playerWins: 0,
   compWins: 0,
   gameLength: 0,
-  newGame: function() {
-    params.output.innerHTML = '';
-    scoresResult();
-    params.outputEndOfGame.innerHTML = '';
-  }
+  progress: []
 };
 
+ // --------------------- Funkcja nowa gra --------------------- \\
+// -------------------------------------------------------------- \\
+var newGame = function() {
+  output.innerHTML = '';
+  scoresResult();
+  outputEndOfGame.innerHTML = '';
+};
+
+ // -------------------- Zdarzenie Nowa Gra -------------------- \\
+// -------------------------------------------------------------- \\
+buttonNewGame.addEventListener('click', function() {
+  resetGame();
+  newGame();
+  params.gameLength = window.prompt('Podaj ilość wygranych rund która zakończy grę!');
+  if (!params.gameLength || isNaN(params.gameLength)) {
+    numberOfGames.innerHTML = '<br> Podaj właściwą liczbę!' + '<br>';
+    disableButtons(true);
+  } else {
+    numberOfGames.innerHTML = '<br> Musisz wygrać: <strong>[ ' + params.gameLength + ' ]</strong> razy aby zakończyć grę!!!';
+    buttonNewGame.disabled = true;
+    disableButtons(false);
+  }
+});
 
  // ---------------- Funkcja wyłączająca buttony --------------- \\
 // -------------------------------------------------------------- \\
 function disableButtons(flag) {
-  params.buttonPaper.disabled = flag;
-  params.buttonRock.disabled = flag;
-  params.buttonScissors.disabled = flag;
+  buttonPaper.disabled = flag;
+  buttonRock.disabled = flag;
+  buttonScissors.disabled = flag;
 }
 disableButtons(true);
 
- // ------------- funkcja która losuje liczb 1-3 ------------- \\
-// ------------------------------------------------------------ \\ // JEST OK - DZIAŁA \\
-var randomNumber = function() {
-  return Math.floor(Math.random() * 3) + 1;
+ // -------------- funkcja która losuje liczb 1-3 -------------- \\
+// -------------------------------------------------------------- \\ // JEST OK - DZIAŁA \\
+var randomMove = function() {
+  var compChoices = ['Paper', 'Rock', 'Scissors'];
+  var compMove = Math.floor(Math.random() * 3);
+  console.log('compChoice: ', compChoices[compMove]);
+  return compChoices[compMove];
 };
 
- // --------- funkcja która zamienia liczbę na tekst --------- \\
-// ------------------------------------------------------------ \\ // JEST OK - DZIAŁA \\
-var numberToText = function(randomNumber) {
-  if (randomNumber == 1) {
-    return 'PAPIER!';
-  } else if (randomNumber == 2) {
-    return 'KAMIEŃ!';
-  } else if (randomNumber == 3) {
-    return 'NOŻYCE!';
-  } 
+ // -------------------- Funkcja playerMove -------------------- \\
+// -------------------------------------------------------------- \\
+var playerMove = function(userMove) { // deklaruję wyrażenie funkcyjne playerMove (parametrem jest userMove)
+  player.choice = userMove;
+  computer.choice = randomMove();
+  var compareResult = compare();
+  resultOutput(compareResult);
+  scoresResult();
+  addRecord();
+  endOfGame();
 };
 
- // funkcja która porównuje wylosowaną liczbę z wyborem gracza \\
-// ------------------------------------------------------------ \\
-var compare = function(userMove, compChoice) {
-  if (userMove === compChoice) {
+ // Wywołanie zdarzenia reagujące po kliknięciu w wybrany button \\
+// -------------------------------------------------------------- \\
+var buttonPlayerMove = document.querySelectorAll('.player-move');
+//console.log('tablica: ', buttonPlayerMove);  
+for (var i = 0; i < buttonPlayerMove.length; i++) {
+  buttonPlayerMove[i].addEventListener('click', function() {
+    //console.log(this.getAttribute('data-move'));
+    playerMove(this.getAttribute('data-move'));
+  });
+}
+
+ // -funkcja która porównuje wylosowaną liczbę z wyborem gracza- \\
+// -------------------------------------------------------------- \\
+var compare = function() {
+  if (player.choice === computer.choice) {
       return 'REMIS!!! ';
-  } else if ((userMove == buttonPlayerMove[0]) && (compChoice == 2) ||
-             (userMove == buttonPlayerMove[1]) && (compChoice == 3) ||
-             (userMove == buttonPlayerMove[2]) && (compChoice == 1)) {
-      params.playerWins++;
+  } else if ((player.choice === 'Paper') && (computer.choice == 'Rock') ||
+             (player.choice === 'Rock') && (computer.choice == 'Scissors') ||
+             (player.choice === 'Scissors') && (computer.choice == 'Paper')) {
+      player.score++;
       return 'WYGRYWASZ :) ';
   } else {
-      params.compWins++;
+      computer.score++;
       return 'PRZEGRYWASZ :( ';
       }
 };
@@ -76,87 +111,98 @@ var compare = function(userMove, compChoice) {
  // -------- Wyświetlanie wyniku pojedyńczej rozgrywki --------- \\
 // -------------------------------------------------------------- \\
 var scoresResult = function() {
-  params.outputResult.innerHTML = 'Gracz >>>> <strong>' + params.playerWins +  ' vs ' + params.compWins + '</strong> <<<< Komputer <br><br>';
+  outputResult.innerHTML = 'Gracz >>>> <strong>' + player.score +  ' vs ' + computer.score + '</strong> <<<< Komputer <br><br>';
 };
-
 
  // ------------ Funkcja resetująca punktację graczy ----------- \\
 // -------------------------------------------------------------- \\
 var resetGame = function() {
-  params.playerWins = 0;
-  params.compWins = 0;
+  player.score = 0;
+  computer.score = 0;
+  params = {
+    playerWins: 0,
+    compWins: 0,
+    gameLength: 0,
+    progress: [],
+  };
 };
 
- // -------------------- Zdarzenie Nowa Gra -------------------- \\
+ // --- Funkcja do wyświetlania wyniku dla pojedyńczej rundy --- \\
 // -------------------------------------------------------------- \\
-params.buttonNewGame.addEventListener('click', function() {
-  params.newGame();
-  params.gameLength = window.prompt('Podaj ilość wygranych rund która zakończy grę!');
-  if (!params.gameLength || isNaN(params.gameLength)) {
-    params.numberOfGames.innerHTML = '<br> Podaj właściwą liczbę!' + '<br>';
-    disableButtons(true);
-  } else {
-    params.numberOfGames.innerHTML = '<br> Musisz wygrać: <strong>[ ' + params.gameLength + ' ]</strong> razy aby zakończyć grę!!!';
-    params.buttonNewGame.disabled = true;
-    disableButtons(false);
-  }
-});
-
- // -------------------- Funkcja playerMove -------------------- \\
-// -------------------------------------------------------------- \\
-var playerMove = function(userMove) {
-  var compChoice = randomNumber();
-  var compareResult = compare(userMove, compChoice);
-  var myText = numberToText(userMove);
-  var compText = numberToText(compChoice);
-  resultOutput(compareResult, myText, compText);
-  scoresResult();
-  endOfGame();
-};
-
- // Wywołanie zdarzenia reagujące po kliknięciu w wybrany button \\
-// -------------------------------------------------------------- \\
-var buttonPlayerMove = document.querySelectorAll('.player-move');
-console.log('tablica: ', buttonPlayerMove);
-for (var i = 0; i < buttonPlayerMove.length; i++) {
-  //console.log('i: ', buttonPlayerMove);
-  var btnDataMove = buttonPlayerMove[i].getAttribute('data-move');
-  //console.log('button: ', btnDataMove);
-  buttonPlayerMove[i].addEventListener('click', function() {
-    console.log('btn: ', btnDataMove);
-    //console.log(this);
-    playerMove(btnDataMove);
-    });
-  }
-  
-/*buttonPaper.addEventListener('click', function() { 
-  playerMove(1);
-});
-
-buttonRock.addEventListener('click', function() { 
-  playerMove(2);
-});
-
-buttonScissors.addEventListener('click', function() { 
-  playerMove(3);
-});*/
-
-// ---- Funkcja do wyświetlania wyniku dla pojedyńczej rundy --- \\
-// -------------------------------------------------------------- \\
-function resultOutput(compareResult, myText, compText) {
-  params.output.innerHTML = compareResult + "wybrałeś: " + myText + " komputer wybrał: " + compText + " <br><br>";
+function resultOutput(compareResult) {
+  output.innerHTML = compareResult + "wybrałeś: " + player.choice + " komputer wybrał: " + computer.choice + " <br><br>";
 }
 
 function endOfGame() {
-  if (params.gameLength == params.playerWins) {
-    params.buttonNewGame.disabled = false;
+  if (params.gameLength == player.score) {
+    buttonNewGame.disabled = false;
     disableButtons(true);
-    resetGame();
-    params.outputEndOfGame.innerHTML = '<strong>WYGRYWASZ</strong> TEN POJEDYNEK!!! <br><br> ABY ZAGRAĆ PONOWNIE WYBIERZ: <strong>NOWA GRA</strong>';
-  } else if (params.gameLength == params.compWins) {
-    params.buttonNewGame.disabled = false;
+    createModal();
+    showModal();
+    outputEndOfGame.innerHTML = '<strong>WYGRYWASZ</strong> TEN POJEDYNEK!!! <br><br> ABY ZAGRAĆ PONOWNIE WYBIERZ: <strong>NOWA GRA</strong>';
+  } else if (params.gameLength == computer.score) {
+    buttonNewGame.disabled = false;
     disableButtons(true);
-    resetGame();
-    params.outputEndOfGame.innerHTML = '<strong>PRZEGRYWASZ !!!</strong> KOMPUTER WYGRAŁ TEN POJEDYNEK!!! <br><br> ABY ZAGRAĆ PONOWNIE WYBIERZ: <strong>NOWA GRA</strong>';
+    createModal();
+    showModal();
+    outputEndOfGame.innerHTML = '<strong>PRZEGRYWASZ !!!</strong> KOMPUTER WYGRAŁ TEN POJEDYNEK!!! <br><br> ABY ZAGRAĆ PONOWNIE WYBIERZ: <strong>NOWA GRA</strong>';
   }
 }
+
+function createModal() {
+  var modal = document.querySelectorAll('.modal');
+  var newHTML = '<table><thead><tr><th>Rounds | </th><th>Your Move | </th><th>Computer Move | </th><th>Round Result | </th><th><a href="#" class="close">X</a></th></tr></thead><tbody>';
+  for (i = 0; i < params.progress.length; i++) {
+      newHTML += '<tr><td>' +
+          params.progress[i].rounds + '</td><td>' +
+          params.progress[i].playerChoice + '</td><td>' +
+          params.progress[i].computerChoice + '</td><td>' +
+          params.progress[i].playerScore + ' : ' + params.progress[i].computerScore + '</td></tr>'
+  }
+  newHTML += '</tbody></table>';
+  console.log(newHTML);
+  modal[0].innerHTML = newHTML;
+}
+
+function addRecord() {
+  params.progress.push({
+      rounds: (params.gameLength),
+      playerScore: (player.score),
+      computerScore: (computer.score),
+      playerChoice: (player.choice),
+      computerChoice: (computer.choice)
+  })
+  console.log('progres: ', params.progress);
+}
+
+//var modal = document.querySelectorAll('.modal');
+var showModal = function(event){
+  // event.preventDefault(); 
+  document.querySelector('#modal-overlay').classList.add('show');
+};
+
+// Dodajemy funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
+
+var hideModal = function(event){
+  //event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+};
+var closeButton = document.querySelectorAll('.modal .close');
+  console.log('closeButton:', closeButton);
+
+closeButton.addEventListener('click', hideModal);
+
+//for(var i = 0; i < closeButtons.length; i++){
+  
+//}
+//zamykania modala poprzez kliknięcie w overlay. 
+
+//document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
+
+/*for(var i = 0; i < modal.length; i++){
+  modal[i].addEventListener('click', function(event){
+    event.stopPropagation();
+  });
+}*/
